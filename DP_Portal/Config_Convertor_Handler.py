a76540eb89e2e7cee84dea38fb1de05d0ea10441
MaorY <yahlomi@gmail.com>
2021-08-25 11:45:55 +0000

    commit

--- DP_Portal/Config_Convertor_Handler.py
+++ DP_Portal/Config_Convertor_Handler.py
@@ -3,11 +3,12 @@ import requests
 import json
 import time
 import timeit
+import math
 from Excel_Handler import Excel_Handler
 
 class Config_Convertor_Handler:
     def __init__(self):
-        self.configuration_book =  Excel_Handler("test.xlsm")
+        self.configuration_book =  Excel_Handler("Policy_Editor_micro_new_V7.xlsm")
 
     def print_table(self,worksheet): 
         print(self.configuration_book.read_table(worksheet))
@@ -24,13 +25,15 @@ class Config_Convertor_Handler:
         multi_sub_index = 0
 
         multi_net_dic = self.configuration_book.check_multi_network()
-        print(multi_net_dic)
         net_class_xl_format = self.configuration_book.read_table("Network Classes")
         for index in range(len(net_class_xl_format)):
-            network_name = self.configuration_book.get_network_name(index)
-            network_subnet = self.configuration_book.get_network_address(index)
-            network_mask = self.configuration_book.get_network_mask(index)
-           
+
+            # network_name = self.configuration_book.get_network_name(index)
+            # network_subnet = self.configuration_book.get_network_address(index)
+            # network_mask = self.configuration_book.get_network_mask(index)
+
+            network_name, network_subnet, network_mask = self.configuration_book.get_network_entry_details(index)
+
             for net_name_key in multi_net_dic.keys():
                 #print(net_name_key)
                 if network_name == net_name_key and key_found == 0:
@@ -57,18 +60,80 @@ class Config_Convertor_Handler:
         #print(list_of_net)
         return net_class_list
 
+    def create_BDoS_Profile_dic(self):
+        BDoS_Profile_list = []
+        net_class_xl_format = self.configuration_book.read_table(
+            "Policy Editor")
+
+        for index in range(len(net_class_xl_format)):
+            BDoS_name,BDos_BW = self.configuration_book.get_BDoS_profile_details(
+                index)
+            if math.isnan(BDos_BW) == False:
+                print(BDoS_name, BDos_BW)
+                BDoS_Profile_list.append(
+                    create_single_BDoS_dic(BDoS_name, int(BDos_BW)))
+        
+        #print(BDoS_Profile_list)
+        return BDoS_Profile_list
+
+def create_single_BDoS_dic(BDoS_Profile_Name, BDoS_Profile_BW):
+    bdos_profile_body = {
+        "rsNetFloodProfileName": f"{BDoS_Profile_Name}_auto",
+  						"rsNetFloodProfilePacketReportStatus": "1",
+  						"rsNetFloodProfileTransparentOptimization": "2",
+  						"rsNetFloodProfileAction": "1",
+  						"rsNetFloodProfileTcpSynStatus": "1",
+  						"rsNetFloodProfileTcpFinAckStatus": "1",
+  						"rsNetFloodProfileTcpRstStatus": "1",
+  						"rsNetFloodProfileTcpSynAckStatus": "1",
+  						"rsNetFloodProfileTcpFragStatus": "1",
+  						"rsNetFloodProfileUdpStatus": "1",
+  						"rsNetFloodProfileUdpFragStatus": "1",
+  						"rsNetFloodProfileIcmpStatus": "1",
+  						"rsNetFloodProfileIgmpStatus": "1",
+  						"rsNetFloodProfileBandwidthIn": BDoS_Profile_BW,
+  						"rsNetFloodProfileBandwidthOut":  BDoS_Profile_BW,
+  						"rsNetFloodProfileTcpInQuota": "0",
+  						"rsNetFloodProfileTcpOutQuota": "0",
+  						"rsNetFloodProfileUdpInQuota": "0",
+  						"rsNetFloodProfileUdpOutQuota": "0",
+  						"rsNetFloodProfileUdpFragInQuota": "0",
+  						"rsNetFloodProfileUdpFragOutQuota": "0",
+  						"rsNetFloodProfileIcmpInQuota": "0",
+  						"rsNetFloodProfileIcmpOutQuota": "0",
+  						"rsNetFloodProfileIgmpInQuota": "0",
+  						"rsNetFloodProfileIgmpOutQuota": "0",
+  						"rsNetFloodProfileLevelOfReuglarzation": "2",
+  						"rsNetFloodProfileUdpExcludedPorts": "None",
+  						"rsNetFloodProfileAdvUdpDetection": "2",
+  						"rsNetFloodProfileAdvUdpLearningPeriod": "2",
+  						"rsNetFloodProfileAdvUdpAttackHighEdgeOverride": "0.0",
+  						"rsNetFloodProfileAdvUdpAttackLowEdgeOverride": "0.0",
+  						"rsNetFloodProfileBurstEnabled": "1",
+  						"rsNetFloodProfileNoBurstTimeout": "30",
+  						"rsNetFloodProfileOverMitigationStatus": "2",
+  						"rsNetFloodProfileOverMitigationThreshold": "25",
+  						"rsNetFloodProfileLearningSuppressionThreshold": "25",
+  						"rsNetFloodProfileFootprintStrictness": "1",
+  						"rsNetFloodProfileRateLimit": "0",
+  						"rsNetFloodProfileUserDefinedRateLimit": "0",
+  						"rsNetFloodProfileUserDefinedRateLimitUnit": "0"
+    }
+    return bdos_profile_body
+
 def create_single_net_dic(network_name, netowrk_subnet, sub_index, net_mask):
 
     single_net_class_dic = {
-        "rsBWMNetworkName": network_name,
+        "rsBWMNetworkName": f"{network_name}_auto",
         "rsBWMNetworkSubIndex": sub_index,
         "rsBWMNetworkMode": "1",
         "rsBWMNetworkAddress": netowrk_subnet,
         "rsBWMNetworkMask": net_mask
     }
     return single_net_class_dic
+    
 
-
-# d1 = Config_Convertor_Handler()
-# #d1.print_table("Network Classes")
-# d1.create_net_class_list()
+d1 = Config_Convertor_Handler()
+#d1.print_table("Network Classes")
+#d1.create_net_class_list()
+#d1.create_BDoS_Profile_dic()
--- DP_Portal/Excel_Handler.py
+++ DP_Portal/Excel_Handler.py
@@ -16,20 +16,24 @@ class Excel_Handler:
         return table
         #print(table_sheet)
 
-    def get_network_name(self,index):
-        net_class_xl_format = self.read_table("Network Classes")
-        #print(net_class_xl_format[index]["Network Name"])
-        return net_class_xl_format[index]["Network Name"]
-        
-    def get_network_address(self,index):
-        net_class_xl_format = self.read_table("Network Classes")
-        #print(net_class_xl_format[index]["Network Address"])
-        return net_class_xl_format[index]["Network Address"]
+    # def get_network_name(self,index):
+    #     net_class_xl_format = self.read_table("Network Classes")
+    #     #print(net_class_xl_format[index]["Network Name"])
+    #     return net_class_xl_format[index]["Network Name"]
 
-    def get_network_mask(self,index):
+    def get_network_entry_details(self, index):
         net_class_xl_format = self.read_table("Network Classes")
-        #print(net_class_xl_format[index]["Mask"])
-        return net_class_xl_format[index]["Mask"]
+        return net_class_xl_format[index]["Network Name"], net_class_xl_format[index]["Network Address"], net_class_xl_format[index]["Mask"]
+
+    # def get_network_address(self,index):
+    #     net_class_xl_format = self.read_table("Network Classes")
+    #     #print(net_class_xl_format[index]["Network Address"])
+    #     return net_class_xl_format[index]["Network Address"]
+
+    # def get_network_mask(self,index):
+    #     net_class_xl_format = self.read_table("Network Classes")
+    #     #print(net_class_xl_format[index]["Mask"])
+    #     return net_class_xl_format[index]["Mask"]
 
     def check_multi_network(self):
         net_class_xl_format = self.read_table("Network Classes")
@@ -42,27 +46,14 @@ class Excel_Handler:
         #print(multi_net_dic)
         return multi_net_dic
 
+    def get_BDoS_profile_details(self,index):
+        net_class_xl_format = self.read_table("Policy Editor")
+        BDoS_profile_name = net_class_xl_format[index]["Policy Name"]
+        BDoS_profile_BW= net_class_xl_format[index]["Policy BW"]
+        return BDoS_profile_name, BDoS_profile_BW
+        print(net_class_xl_format[index]["Policy Name"])
+        #return net_class_xl_format[index]["Network Name"]
 
-        #network_name = net_class_xl_format[index]["Network Name"]
-        network_name_mask = [net_class_xl_format[index]["Network Address"]
-                                for index in range(len(net_class_xl_format))]
-        #return network_name_address
-        print(network_name_mask)
-
-
-
-def create_single_net_dic(network_name, netowrk_subnet,index,net_mask):
-
-        single_net_class_dic = {
-            "rsBWMNetworkName": network_name,
-            "rsBWMNetworkSubIndex": index,
-            "rsBWMNetworkMode": "1",
-            "rsBWMNetworkAddress": netowrk_subnet,
-            "rsBWMNetworkMask": net_mask
-        }
-        return single_net_class_dic
-
-
-v1 = Excel_Handler("test.xlsm")
+v1 = Excel_Handler("Policy_Editor_micro_new_V7.xlsm")
 #v1.create_net_class_dic()
-#v1.get_network_name()
+#v1.get_BDoS_profile_details()
