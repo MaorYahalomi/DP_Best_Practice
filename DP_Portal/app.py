914cc5d3e2eddc259fb2e68d5c592a7140336355
MaorY <yahlomi@gmail.com>
2021-11-03 10:23:21 +0000

    Adding_Policy_Priority

---
+++ DP_Portal/app.py
@@ -0,0 +1,17 @@
+import getpass
+from vision import *
+#from vision import BP_Tool_run
+# MAIN Prog Tests#
+
+if __name__ == "__main__":
+
+ Vision_IP = input("Enter Vision IP: ")
+ Vision_user = input("Enter Vision User: ")
+ Vision_password = getpass.getpass("Enter Vision Password: ") 
+ try:
+ 	vision_obj = Vision(Vision_IP, Vision_user, Vision_password)
+ except FileNotFoundError:
+ 	print(f"Error: Config File is not Found")
+ 	exit(1) 
+ DefensePro_list = vision_obj.config_file.get_dp_list()
+ BP_Tool_run(vision_obj,DefensePro_list)
